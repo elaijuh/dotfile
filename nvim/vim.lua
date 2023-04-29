4707d6f8599b8c45e6aec0da8f31c42719057161
hjl <hjiale@gmail.com>
2023-04-30 01:15:33 +0800

    i3, nvim

--- nvim/vim.lua
+++ nvim/vim.lua
@@ -177,30 +177,44 @@ lsp['gopls'].setup({
 -- nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+local function nvimtree_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer=bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', 'P', function()
+    local node = api.tree.get_node_under_cursor()
+    print(node.absolute_path)
+  end, opts('Print Node Path'))
+  vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+end
+
 require("nvim-tree").setup({
-   sort_by = "case_sensitive",
-   view = {
-      adaptive_size = true,
-      mappings = {
-         list = {
-          { key = "u", action = "dir_up" },
-         },
+  on_attach = nvimtree_on_attach,
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+  },
+  renderer = {
+    group_empty = true,
+    icons = {
+      show = {
+        folder = false,
+        folder_arrow = false,
+        file = false,
+        git = true,
       },
-   },
-   renderer = {
-      group_empty = true,
-      icons = {
-         show = {
-            folder = false,
-            folder_arrow = false,
-            file = false,
-            git = true,
-         },
-      },
-   },
-   filters = {
-      dotfiles = true,
-   },
+    },
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
 
 -- gitsigns
