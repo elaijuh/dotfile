5a894999e10b9888b07f8840c233ae8cda20759f
hjl <hjiale@gmail.com>
2024-05-27 13:35:12 +0800

    refact big

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -14,7 +14,8 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --no-ignore --exclude .git"
-export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 --color=fg:#d9d8d8,bg:#333132,hl:#357172 --color=fg+:#cfd2d0,bg+:#357172,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#cfd2d0 --color=marker:#87ff00,spinner:#af5fff,header:#87afaf"
+# export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 --color=fg:#d9d8d8,bg:#333132,hl:#357172 --color=fg+:#cfd2d0,bg+:#357172,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#cfd2d0 --color=marker:#87ff00,spinner:#af5fff,header:#87afaf"
+export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1"
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
