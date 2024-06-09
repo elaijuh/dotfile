5ace3d19c700b09fdf51b84d071c7354576f733a
hjl <hjiale@gmail.com>
2024-06-09 22:06:57 +0800

    up: ellisonleao/gruvbox.nvim

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -19,14 +19,21 @@ return {
     },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
+
       lspconfig.bashls.setup({
         capabilities = capabilities,
       })
 
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
       })
 
       lspconfig.gopls.setup({
