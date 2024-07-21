e19623b71eda47075b3d1f9a93286bb0df4b9b76
hjl <hjiale@gmail.com>
2024-07-22 00:04:12 +0800

    up: nvim/.config/nvim/lua/plugins/oil.lua

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,28 @@ return {
   "stevearc/oil.nvim",
   config = function()
     local oil = require("oil")
-    oil.setup()
-    vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+    local detail = false
+    oil.setup({
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              oil.set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              oil.set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        preview_split = "right",
+      },
+    })
+    vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open parent directory" })
   end,
 }
