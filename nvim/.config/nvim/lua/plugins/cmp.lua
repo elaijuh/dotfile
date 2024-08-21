0e88601c5886f9cb5c5397356eb192b57f234bfa
hjl <hjiale@gmail.com>
2024-08-22 01:50:10 +0800

    modify nvim/.config/nvim/lua/plugins/cmp.lua

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -21,6 +21,9 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
+        completion = {
+          autocomplete = false,
+        },
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
