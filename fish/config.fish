ae2eeeade3200a814468e57f8562cb4389bd79dd
hjl <hjiale@gmail.com>
2025-12-20 20:43:17 +0800

    sync

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,6 @@ set fish_greeting ""
 
 fish_add_path -m ~/.local/bin ~/go/bin /usr/local/go/bin
 
-set -gx http_proxy 'http://127.0.0.1:7890'
-set -gx https_proxy 'http://127.0.0.1:7890'
 # set -gx http_proxy 'http://127.0.0.1:7897'
 # set -gx https_proxy 'http://127.0.0.1:7897'
 
@@ -24,11 +22,10 @@ if status is-interactive
     end
 end
 
-
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '/home/hjl/.opam/opam-init/init.fish' && source '/home/hjl/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r '/home/hjl/.opam/opam-init/init.fish' && source '/home/hjl/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 # END opam configuration
